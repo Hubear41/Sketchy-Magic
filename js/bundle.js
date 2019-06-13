fd86752468c7e58b913dce631826b41c9923d831
Hubear41 <ddh241@gmail.com>
2019-06-12 20:39:41 -0400

    create checkIfCircle method

--- js/bundle.js
+++ js/bundle.js
@@ -1,4 +1,211 @@
-import Spell from './Spell';
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./js/entry.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./js/Spell.js":
+/*!*********************!*\
+  !*** ./js/Spell.js ***!
+  \*********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+class Spell {
+    constructor({points, position, shape}) {
+        this.shape = shape;
+        this.position = position;
+        this.points = points;
+        this.life = 1;
+        this.decay = 0.1;
+    }
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Spell);
+
+/***/ }),
+
+/***/ "./js/entry.js":
+/*!*********************!*\
+  !*** ./js/entry.js ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _game__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./game */ "./js/game.js");
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let paperCanvas = document.getElementById('paperCanvas');
+    let mainCanvas = document.getElementById('mainCanvas');
+    paper.setup(paperCanvas);
+
+    const game = new _game__WEBPACK_IMPORTED_MODULE_0__["default"](mainCanvas, paperCanvas);
+    game.start();
+});
+
+
+
+/***/ }),
+
+/***/ "./js/game.js":
+/*!********************!*\
+  !*** ./js/game.js ***!
+  \********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _shape_finder__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./shape_finder */ "./js/shape_finder.js");
+
+
+class Game {
+    constructor(canvas, paper) {
+        this.canvas = canvas;
+        this.paper = paper; 
+
+        this.mouseTool = new Tool();
+        this.spellFinder = new _shape_finder__WEBPACK_IMPORTED_MODULE_0__["default"](this.mouseTool, mainCanvas);
+
+        // this.drawBg = this.drawBg.bind(this);
+
+        document.addEventListener('mouseup', () => {
+            this.spell = this.spellFinder.currentSpell;
+        });
+    }
+
+    start() {
+        setInterval(this.draw, 1000);
+    }
+
+    drawBg() {
+        const ctx = this.canvas.getContext('2d');
+
+        ctx.beginPath();
+        ctx.rect(canvas.width / 2, canvas.height / 2, 20, 10);
+        ctx.fillStyle = "#C1B8B6";
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    drawSpells() {
+        if (!this.spell) {
+            return
+        }
+
+        // this.spell.draw();
+        // this.spell.decreaseLife();
+    }
+
+    draw() {
+        this.drawBg();
+        this.drawSpells();
+    }
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Game);
+
+/***/ }),
+
+/***/ "./js/shape_finder.js":
+/*!****************************!*\
+  !*** ./js/shape_finder.js ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _Spell__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Spell */ "./js/Spell.js");
+
 
 class ShapeFinder {
     constructor(tool, canvas) {
@@ -58,7 +265,7 @@ class ShapeFinder {
                 shape: this.shape,
             }
 
-            this.currentSpell = new Spell(spellAttr);
+            this.currentSpell = new _Spell__WEBPACK_IMPORTED_MODULE_0__["default"](spellAttr);
         }
     }
 
@@ -326,4 +533,9 @@ class ShapeFinder {
     }
 }
 
-export default ShapeFinder;
+/* harmony default export */ __webpack_exports__["default"] = (ShapeFinder);
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=bundle.js.map
