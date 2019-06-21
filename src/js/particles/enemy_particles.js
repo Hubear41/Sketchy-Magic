03bca43a67029ebe1543e167b01b71cb95ca049a
Dennis Hu <ddh241@gmail.com>
2019-06-21 11:50:59 -0400

    update styling

--- src/js/particles/enemy_particles.js
+++ src/js/particles/enemy_particles.js
@@ -1,53 +1,44 @@
 class EnemyParticles {
-    constructor( x, y ) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.lifespan = 7;
-        this.decay = 0.5;
+        this.opacity = 1.0;
+        this.decay = Math.random() * (0.1) + 0.05;
         this.delta = {
-            dx: (Math.random() * 2) - 1,
-            dy: (Math.random() * 2) - 1,
+            dx: (Math.random() * 4) - 2,
+            dy: (Math.random() * 4) - 2,
         };
 
-        this.size = 50;
+        this.size = Math.random() * 3 + 1;
         this.state;
-        this.setupSprite();
+        this.color = this._decideColor();
     }
 
     draw(ctx) {
-        const currentFrame = Math.floor((7 - this.lifespan) + 1);
-        const spriteSize = 34;
-        let spriteX, spriteY;
+        ctx.beginPath();
+        ctx.fillStyle = `rgba(${this.color.r},${this.color.g},${this.color.b},${this.opacity})`;
+        ctx.fillRect(this.x, this.y, this.size, this.size);
+        ctx.closePath();
 
-        if ( currentFrame === 1 ) {
-            spriteX = 7 * spriteSize;
-            spriteY = 3 * spriteSize;
-        } else {
-            spriteX = (currentFrame - 2) * spriteSize;
-            spriteY = 4 * spriteSize;
-        }
+        this.opacity -= this.decay;
+        this.x += this.delta.dx;
+        this.y += this.delta.dy;
 
-        ctx.drawImage(  this.explosionImage,
-                        spriteX,
-                        spriteY,
-                        spriteSize,
-                        spriteSize,
-                        this.x - this.size / 2,
-                        this.y - this.size / 2,
-                        this.size,
-                        this.size
-        );
-        
-        this.lifespan -= this.decay;
-
-        if (this.lifespan <= 0) {
+        if (this.opacity <= 0) {
             this.state = 'DONE';
         }
     }
 
-    setupSprite() {
-        this.explosionImage = new Image();
-        this.explosionImage.src = 'assets/Explosions/FX.png';
+    _decideColor() {
+        const COLORS = [
+            { r: 164, g: 42, b: 42 },
+            { r: 244, g: 229, b: 66 },
+            { r: 188, g: 121, b: 33 },
+            { r: 245, g: 252, b: 32 },
+        ];
+
+        const idx = Math.floor(Math.random() * 3);
+        return COLORS[idx];
     }
 }
 
